915e56a5e4e37959912350364440fdb971a6d103
Memnoc <smartdroidesign@gmail.com>
2019-02-25 15:43:56 +0000

    Java - implemented BaseActivity

---
+++ app/src/main/java/com/smartdroidesign/flickrbrowser/BaseActivity.java
@@ -0,0 +1,31 @@
+package com.smartdroidesign.flickrbrowser;
+
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+// Class to store common patterns with other classes
+public class BaseActivity extends AppCompatActivity {
+    private static final String TAG = "BaseActivity";
+    static final String FLICKR_QUERY = "FLICKR_QUERY";
+    static final String PHOTO_TRANSFER = "PHOTO_TRANSFER";
+
+    void activteToolbar(boolean enableHome) {
+        Log.d(TAG, "activteToolbar: starts");
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            Toolbar toolbar = findViewById(R.id.toolbar);
+
+            if (toolbar != null) {
+                setSupportActionBar(toolbar);
+                actionBar = getSupportActionBar();
+            }
+        }
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(enableHome);
+        }
+    }
+}
